dde0428508665c4bb816c8c7e917896ffcbd60b8
Jose26398 <jsg26398@gmail.com>
2019-05-14 00:04:41 +0200

    P3 añadida

---
+++ P3/Codigo.py
@@ -0,0 +1,13 @@
+"""
+TRABAJO 3
+Nombre Estudiante: Jose Maria Sanchez Guerrero
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Fijamos la semilla
+np.random.seed(1)
+
+print('\n\n\nEJERCICIO SOBRE LA COMPLEJIDAD DE H Y EL RUIDO')
+
